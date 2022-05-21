75ad01380986acec41c810522cf275cf4bb53ed2
sreevyshnavi02 <75737654+sreevyshnavi02@users.noreply.github.com>
2022-05-21 19:26:43 +0530

    Time: 13 ms (7.83%), Space: 9.8 MB (23.62%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int begin = 0;
-        int end = nums.size() - 1;
-        int mid;
+        int begin = 0, end = nums.size() - 1, mid;
         while(begin <= end)
         {
             mid = (begin + end)/2;
             if(nums[mid] == target)
-            {
                 return mid;
-            }
             else if(nums[mid] > target)
-            {
                 end = mid - 1;
-            }
             else
-            {
                 begin = mid + 1;
-            }
         }
-        if(nums[mid] > target)
-        {
-            return mid;
-        }
-        else
-        {
-            return mid + 1;
-        }
-        // return mid + 1; // change this
+        
+        return (nums[mid] > target)? mid : mid + 1;
     }
 };
