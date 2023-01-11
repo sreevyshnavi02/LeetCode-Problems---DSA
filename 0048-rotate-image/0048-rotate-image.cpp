4204f7c6943d764fcbf6359fd7301fd10b7358b7
Sree Vyshnavi <75737654+sreevyshnavi02@users.noreply.github.com>
2023-01-11 20:48:45 +0530

    Time: 3 ms (61.15%), Space: 7.2 MB (37.91%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& m) {
-        // with another 2d matrix for rotation - for trial
-        int n = m[0].size();
-        vector<vector<int>> r(n, vector<int>(n));
-        // cout << r.size() << " " << r[0].size() << endl;
-        // cout << "n = " << n << endl;
         
+        // transpose
+        int n = m.size();
         for(int i = 0; i < n; i++){
-            // cout << " r = " << r.size() << ", " << r[i].size() << endl;
-            for(int j = 0; j < n; j++){
-                // cout << "m[i][j] = " << m[i][j] << endl;
-                // cout << j << ", "<< n - 1 - i << endl;
-                r[j][n -1 - i] = m[i][j];
-                // cout << "here" << endl;
+            for(int j = i; j < n; j++){
+                swap(m[i][j], m[j][i]);
             }
         }
         
-        m = r;
+        // reverse each row in the transpose
+        // 2 pointer method
+        
+        for(int i = 0; i < n; i++){
+            cout << "i = " << i << endl;
+            int left = 0;
+            int right = n - 1;
+            while(left < right){
+                swap(m[i][left], m[i][right]);
+                left++;
+                right--;
+            }
+        }
     }
 };
